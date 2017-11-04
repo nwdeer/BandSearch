123ba8df304970bc62bdd0eaa5f33e4bbd3c88a5
Dmitry Vilner <dmitry.vilner@here.com>
2017-11-04 16:48:38 +0100

    Initial commit - basic project structure.

---
+++ app/src/main/java/search/band/vilner/dmitry/bandsearch/MainActivity.java
@@ -0,0 +1,13 @@
+package search.band.vilner.dmitry.bandsearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
