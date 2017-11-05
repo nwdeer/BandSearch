37ea899759ae925f33935d7aaa785b98246d1386
Dmitry Vilner <dmitry.vilner@here.com>
2017-11-05 12:55:04 +0100

    Fixed device rotation on the BandSearchScreen

--- app/src/main/java/search/band/vilner/dmitry/bandsearch/ui/BandSearchActivity.java
+++ app/src/main/java/search/band/vilner/dmitry/bandsearch/ui/BandSearchActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,9 +23,10 @@ public class BandSearchActivity extends AppCompatActivity {
 
     private static final String TAG = BandSearchActivity.class.getSimpleName();
     public static final String SEARCH_TERM = "SearchTerm";
+    private static final String LAST_SEARCH_EXTRA = "last_search";
     private BandsListAdapter bandsListAdapter;
     private static final int BAND_SEARCH_LOADER_ID = 1;
-
+    private String searchTerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +34,15 @@ public class BandSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SearchView searchView = findViewById(R.id.search_bar);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
                 Log.i(TAG, "onQueryTextSubmit: " + s);
-                Bundle bundle = new Bundle();
-                bundle.putString(SEARCH_TERM, s);
                 bandsListAdapter.clear();
                 bandsListAdapter.notifyDataSetChanged();
-                getLoaderManager().restartLoader(BAND_SEARCH_LOADER_ID, bundle, callbacks);
+                BandSearchActivity.this.searchTerm = s;
+                getLoaderManager().restartLoader(BAND_SEARCH_LOADER_ID, null, callbacks);
                 return true;
             }
 
@@ -63,12 +65,25 @@ public class BandSearchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        if (savedInstanceState != null) {
+            String lastSearch = savedInstanceState.getString(LAST_SEARCH_EXTRA);
+            searchTerm = lastSearch;
+            if (!TextUtils.isEmpty(lastSearch)) {
+                getLoaderManager().restartLoader(BAND_SEARCH_LOADER_ID, null, callbacks);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(LAST_SEARCH_EXTRA, searchTerm);
+        super.onSaveInstanceState(outState);
     }
 
     private LoaderManager.LoaderCallbacks<LoaderResult<SearchData>> callbacks = new LoaderManager.LoaderCallbacks<LoaderResult<SearchData>>() {
         @Override
         public Loader<LoaderResult<SearchData>> onCreateLoader(int i, Bundle bundle) {
-            String searchTerm = bundle.getString(SEARCH_TERM);
             return new BandListLoader(BandSearchActivity.this, searchTerm);
         }
 
